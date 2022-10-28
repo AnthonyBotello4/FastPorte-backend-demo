98335c3e4fdd407a287009f86942d309e6bfa530
Anthony Botello S <89035933+AnthonyBotello4@users.noreply.github.com>
2022-10-28 13:56:35 -0500

    create: base project

---
+++ src/main/java/com/fastporte/fastportedemo/FastPorteDemoApplication.java
@@ -0,0 +1,13 @@
+package com.fastporte.fastportedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastPorteDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastPorteDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fastporte/fastportedemo/FastPorteDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastporte.fastportedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastPorteDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
